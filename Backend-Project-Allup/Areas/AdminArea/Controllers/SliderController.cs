8dba90e05cdcf09dc1e17d3be3ff76904341bf7b
HabibIbrahimov <habibyi@code.edu.az>
2022-02-04 23:43:46 +0400

    feat:creatslider in adminpanel

---
+++ Backend-Project-Allup/Areas/AdminArea/Controllers/SliderController.cs
@@ -0,0 +1,68 @@
+﻿using Backend_Project_Allup.DAL;
+using Backend_Project_Allup.Extensions;
+using Backend_Project_Allup.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Project_Allup.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+
+    public class SliderController : Controller
+    {
+        private readonly Context _context;
+        private readonly IWebHostEnvironment _env;
+        public SliderController(Context context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+            return View(sliders);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Slider slider)
+        {
+            if (ModelState["Photo"].ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
+            {
+                ModelState.AddModelError("Photo", "Don't empty");
+            }
+
+            if (!slider.Photo.IsImage())
+            {
+                ModelState.AddModelError("Photo", "only image");
+                return View();
+            }
+            if (slider.Photo.IsCorrectSize(500))
+            {
+                ModelState.AddModelError("Photo", "500-den yuxari ola bilmez!");
+                return View();
+            }
+
+
+            Slider newSlider = new Slider();
+
+            string fileName = await slider.Photo.SaveImageAsync(_env.WebRootPath, "assets/images");
+            newSlider.ImageUrl = fileName;
+
+
+            await _context.Sliders.AddAsync(newSlider);
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- Backend-Project-Allup/Models/Slider.cs
+++ Backend-Project-Allup/Models/Slider.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +11,10 @@ namespace Backend_Project_Allup.Models
     public class Slider
     {
         public int Id { get; set; }
+        [StringLength(260),MinLength(5)]
         public string ImageUrl { get; set; }
+        [NotMapped]
+        [Required]
+        public IFormFile Photo { get; set; }
     }
 }
