2ee5aae427c7651ff8e8a3f563c0f753f8459a28
HabibIbrahimov <habibyi@code.edu.az>
2022-02-06 16:46:00 +0400

    feat:created forgot password

--- Backend-Project-Allup/Controllers/AccountController.cs
+++ Backend-Project-Allup/Controllers/AccountController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Backend_Project_Allup.Controllers
@@ -102,6 +104,84 @@ namespace Backend_Project_Allup.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> ForgetPassword(ForgetPassword model)
+        {
+            AppUser user = await _userManager.FindByEmailAsync(model.User.Email);
+            if (user == null) return NotFound();
+            var token = _userManager.GeneratePasswordResetTokenAsync(user);
+            var link = Url.Action(nameof(ResetPassword), "Account", new { email = user.Email, token = token }, Request.Scheme);
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress("loremipsump125@gmail.com", "Reset");
+                mail.To.Add(user.Email);
+                mail.Subject = "Reset Password";
+                mail.Body = $"<async href={ link}>Go to Reset Password View</a>";
+                mail.IsBodyHtml = true;
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("loremipsump125@gmail.com", "12345@Lm");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public async Task<IActionResult> ResetPassword(string email, string token)
+        {
+            AppUser user = await _userManager.FindByEmailAsync(email);
+            if (user == null) return NotFound();
+            ForgetPassword forgetPassword = new ForgetPassword
+            {
+                Token = token,
+                User = user
+            };
+            return View(forgetPassword);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        [ActionName("ResetPassword")]
+        public async Task<IActionResult> ResetPassword(ForgetPassword model)
+        {
+            AppUser user = await _userManager.FindByEmailAsync(model.User.Email);
+            if (user == null) return NotFound();
+            ForgetPassword forgetPassword = new ForgetPassword
+            {
+                Token = model.Token,
+                User = user
+            };
+            if (!ModelState.IsValid) return View(forgetPassword);
+            IdentityResult result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
     }
---
+++ Backend-Project-Allup/ViewModels/ForgetPassword.cs
@@ -0,0 +1,19 @@
+﻿using Backend_Project_Allup.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Project_Allup.ViewModels
+{
+    public class ForgetPassword
+    {
+        public AppUser User { get; set; }
+        public string Token { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
