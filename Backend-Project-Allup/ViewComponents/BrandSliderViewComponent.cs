660634f5abe0c03046308357fc30baf1b5902717
HabibIbrahimov <habibyi@code.edu.az>
2022-02-08 14:20:31 +0400

    feat:brandslider and subscribe added in viewcomponent

---
+++ Backend-Project-Allup/ViewComponents/BrandSliderViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Backend_Project_Allup.DAL;
+using Backend_Project_Allup.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Project_Allup.ViewComponents
+{
+    public class BrandSliderViewComponent : ViewComponent
+    {
+        private readonly Context _context;
+        public BrandSliderViewComponent(Context context)
+        {
+            _context = context;
+
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            List<BrandSlider> brandSliders = _context.BrandSliders.ToList();
+            return View(await Task.FromResult(brandSliders));
+
+        }
+    }
+}
---
+++ Backend-Project-Allup/ViewComponents/SubscribeViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Backend_Project_Allup.DAL;
+using Backend_Project_Allup.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Project_Allup.ViewComponents
+{
+    public class SubscribeViewComponent: ViewComponent
+    {
+        private readonly Context _context;
+
+        public SubscribeViewComponent(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Subscribe subscribe = _context.Subscribes.FirstOrDefault();
+            return View(await Task.FromResult(subscribe));
+        }
+    }
+}
