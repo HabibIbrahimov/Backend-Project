f8e150f9e40fd641c2c551ad4059139b1c38be40
HabibIbrahimov <habibyi@code.edu.az>
2022-02-10 14:17:53 +0400

    feat:BrandCreat in adminpanel

---
+++ Backend-Project-Allup/Areas/AdminArea/Controllers/BrandController.cs
@@ -0,0 +1,63 @@
+﻿using Backend_Project_Allup.DAL;
+using Backend_Project_Allup.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Project_Allup.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class BrandController : Controller
+    {
+        private readonly Context _context;
+
+        public BrandController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Brand> brands = _context.Brands.Include(b => b.CategoryBrand).ThenInclude(c => c.Category).ToList();
+            return View(brands);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.IsMainCategory = _context.Categories.Where(c => c.IsMain == true).ToList();
+            ViewBag.SubCategory = _context.Categories.Where(c => c.IsMain == false).ToList();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Brand brand, int[] subcategory)
+        {
+            bool isExist = _context.Brands.Any(c => c.Name.ToLower() == brand.Name.ToLower().Trim());
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "This category is already exists");
+                return RedirectToAction("Index");
+
+            }
+            if (subcategory != null)
+            {
+                Brand newBrand = new Brand();
+                newBrand.Name = brand.Name;
+                await _context.Brands.AddAsync(newBrand);
+                await _context.SaveChangesAsync();
+
+                foreach (var item in subcategory)
+                {
+                    CategoryBrand categoryBrands = new CategoryBrand();
+                    categoryBrands.BrandId = newBrand.Id;
+                    categoryBrands.CategoryId = item;
+                    await _context.AddAsync(categoryBrands);
+                    await _context.SaveChangesAsync();
+                }
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
